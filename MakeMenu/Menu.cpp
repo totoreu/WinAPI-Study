be7fb3343d201c5cb30b510bfc39452f9a9e3ad7
God_win <96413770+totoreu@users.noreply.github.com>
2023-11-22 23:50:36 +0900

    리소스 메뉴 만들어 적용시키기

---
+++ MakeMenu/Menu.cpp
@@ -0,0 +1,78 @@
+#include <Windows.h>
+#include "resource.h"
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("Menu");
+
+int APIENTRY WinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpszCmdParam,
+    _In_ int nCmdShow)
+{
+    HWND hWnd;
+    MSG Message;
+    WNDCLASS WndClass;
+    g_hInst = hInstance;
+
+    WndClass.cbClsExtra = 0;
+    WndClass.cbWndExtra = 0;
+    WndClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    WndClass.hInstance = hInstance;
+    WndClass.lpfnWndProc = WndProc;
+    WndClass.lpszClassName = lpszClass;
+    WndClass.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
+    WndClass.style = CS_HREDRAW | CS_VREDRAW;
+    RegisterClass(&WndClass);
+
+    hWnd = CreateWindow(
+        lpszClass,
+        TEXT("Menu Test Window"),
+        WS_OVERLAPPEDWINDOW | WS_VSCROLL,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        300, 100,
+        NULL,
+        (HMENU)NULL,
+        hInstance,
+        NULL
+    );
+
+    ShowWindow(hWnd, nCmdShow);
+
+    while (GetMessage(&Message, NULL, 0, 0))
+    {
+        TranslateMessage(&Message);
+        DispatchMessage(&Message);
+    }
+    return (int)Message.wParam;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+    switch (iMessage)
+    {
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+        case ID_FILE_MENU1:
+            MessageBox(hWnd, TEXT("Menu1 클릭"), TEXT("메뉴 클릭 확인 창"), MB_OK);
+            break;
+        case ID_FILE_MENU2:
+            MessageBox(hWnd, TEXT("Menu2 클릭"), TEXT("메뉴 클릭 확인 창"), MB_OK);
+            break;
+        case ID_FILE_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        }
+        return 0;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
